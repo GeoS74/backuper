eff9666c2f3b2ae3754b372333360bbc6c4b71f7
geos <gsirotkin@yandex.ru>
2023-10-02 16:28:01 +0500

    start

---
+++ src/main/java/com/signal/backuper/Backuper.java
@@ -0,0 +1,13 @@
+package com.signal.backuper;
+
+/**
+ *
+ * @author geos
+ */
+public class Backuper {
+
+    public static void main(String[] args) throws InterruptedException {
+        App app = new App();
+        app.run();
+    }
+}
