d05631bb4d43f8d18d6b21ba8501148aebe356cb
GeoS74 <gsirotkin@yandex.ru>
2023-10-03 03:57:51 +0300

    start new dev

--- src/main/java/com/signal/backuper/App.java
+++ src/main/java/com/signal/backuper/App.java
@@ -1,31 +1,64 @@
 package com.signal.backuper;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    private static Scanner scan = new Scanner(System.in);
-    private static ArrayList<Path> from = new ArrayList<>();
+    private static final String IGNORE_FILE_NAME = ".backupignore";
+    private static final ArrayList<String> ignoreRules = new ArrayList<>();
+    private static final Scanner scan = new Scanner(System.in);
+    private static Path from;
+    private static Path to;
+    private static Long timeOut;
     
-    public static void addPaths(){
-        System.out.print("input files and directories for backup");
-        System.out.println(" (empty string for the end):");
-        while(true) {
-            String path = App.scan.nextLine();
-            if("".equals(path)) {
-                break;
-            }
-            App.from.add(Paths.get(path));
-        }
-        App.showPathsFrom();
+    public static void start(){
+        
     }
     
-    private static void showPathsFrom() {
-        System.out.println("added files and directories:");
-        for(Path p: App.from) {
-            System.out.println("\t"+p);
+    public static void setTimeout() {
+        System.out.println("input timeout in ms:");
+        int ms = Integer.parseInt(App.scan.nextLine());
+        if(ms < 60*1000) {
+            ms = 60*1000;
+        }
+        App.timeOut = (long) ms;
+    }
+
+    public static void setFrom() throws Exception {
+        System.out.println("input directory for backup:");
+        Path path = Paths.get(App.scan.nextLine());
+        if(!Files.isDirectory(path)) {
+            throw new Exception(path +" is not directory");
+        }
+        App.from = path;
+        App.readIgnoreFile();
+    }
+    
+    public static void setTo() throws Exception {
+        System.out.println("input target directory:");
+        Path path = Paths.get(App.scan.nextLine());
+        if(!Files.isDirectory(path)) {
+            throw new Exception(path + " is not directory");
+        }
+        App.to = path;
+    }
+    
+    private static void readIgnoreFile() {
+        Path path = Paths.get(App.from.toString(), App.IGNORE_FILE_NAME);
+        
+        try {
+            List<String> ignoreRules = Files.readAllLines(path);
+
+            for(String rule: ignoreRules) {
+                App.ignoreRules.add(rule);
+            }
+        } catch (IOException ex) {
+            System.out.println("ATTENTION: file .backupignor not exist");
         }
     }
 
--- src/main/java/com/signal/backuper/Backuper.java
+++ src/main/java/com/signal/backuper/Backuper.java
@@ -1,8 +1,5 @@
 package com.signal.backuper;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  *
@@ -10,11 +7,21 @@ import java.nio.file.Paths;
  */
 public class Backuper {
 
-    public static void main(String[] args) throws InterruptedException {
-        App.addPaths();
-//        String str = "/home/geos/test/from/foo";
+    public static void main(String[] args) {
+        try {
+            App.setFrom();
+            App.setTo();
+            App.setTimeout();
+            App.start();
+            
+//        String str = "/home/geos/test/from";
 //        Path p = Paths.get(str);
 //        System.out.println(Files.isDirectory(p));
 //        System.out.println(Files.isRegularFile(p));
+        } catch (Exception ex) {
+              System.out.println(ex.getMessage());
+              System.exit(0);
+//            Logger.getLogger(Backuper.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
