48b214c49d533fe26b38e4c22e26fb2b72fa34d7
geos <gsirotkin@yandex.ru>
2023-10-03 11:23:11 +0500

    set time start

--- src/main/java/com/signal/backuper/App.java
+++ src/main/java/com/signal/backuper/App.java
@@ -7,25 +7,51 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App {
     private static final String IGNORE_FILE_NAME = ".backupignore";
     private static final ArrayList<String> ignoreRules = new ArrayList<>();
     private static final Scanner scan = new Scanner(System.in);
+    private static final String TIME_PATTERN = "\\d{1,2}:\\d{1,2}";
     private static Path from;
     private static Path to;
     private static Long timeOut;
+    private static final ArrayList<String> timeStart = new ArrayList<>();
     
     public static void start(){
         
     }
     
+    public static void setTimeStart() throws Exception {
+        System.out.println("input time for start backup files (example: \"05:15; 23:10\"):");
+        String time = App.scan.nextLine();
+        Pattern pattern = Pattern.compile(App.TIME_PATTERN);
+        Matcher match = pattern.matcher(time);
+
+        while(match.find()) {
+            App.timeStart.add(time.substring(match.start(), match.end()));
+        }
+        
+        if(App.timeStart.isEmpty()) {
+            throw new Exception("incorrect set time for start");
+        }
+    }
+    
     public static void setTimeout() {
         System.out.println("input timeout in ms:");
-        int ms = Integer.parseInt(App.scan.nextLine());
-        if(ms < 60*1000) {
-            ms = 60*1000;
+        int ms = 0;
+        
+        try {
+            ms = Integer.parseInt(App.scan.nextLine());
+        } 
+        catch (NumberFormatException ex) {} 
+        
+        if(ms < 60*60*1000) {
+            ms = 60*60*1000;
         }
+        
         App.timeOut = (long) ms;
     }
 
--- src/main/java/com/signal/backuper/Backuper.java
+++ src/main/java/com/signal/backuper/Backuper.java
@@ -1,5 +1,13 @@
 package com.signal.backuper;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -9,15 +17,39 @@ public class Backuper {
 
     public static void main(String[] args) {
         try {
-            App.setFrom();
-            App.setTo();
-            App.setTimeout();
-            App.start();
+//            App.setFrom();
+//            App.setTo();
+//            App.setTimeout();
+              App.setTimeStart();
+//            App.start();
+              
+              String str = "08:10";
+              String str2 = "08:10;7:15;22:23; 123:533 45&77";
+              String str3 = "25:35";
+//              
+//              GregorianCalendar cal = new GregorianCalendar(2002, 0, 15, 11, 35);
+//              GregorianCalendar cal2 = new GregorianCalendar(2002, 0, 16);
+//
+//              System.out.println(cal.getTime());
+//              
+//              SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+//              SimpleDateFormat formatter2 = new SimpleDateFormat("HH:mm:ss");
+//               
+//              System.out.println(formatter.format(cal.getTime()));
+//              
+//              System.out.println(formatter.format(new Date()));
+//              
+//              System.out.println(formatter.format(new Date()).equals(formatter.format(new Date())));
+
+//                Pattern pattern = Pattern.compile("\\d{1,2}:\\d{1,2}");
+//                Matcher match = pattern.matcher(str2);
+//                while(match.find()) {
+//                    System.out.println(str2.substring(match.start(), match.end()));
+//                }
+       
+     
+               
             
-//        String str = "/home/geos/test/from";
-//        Path p = Paths.get(str);
-//        System.out.println(Files.isDirectory(p));
-//        System.out.println(Files.isRegularFile(p));
         } catch (Exception ex) {
               System.out.println(ex.getMessage());
               System.exit(0);
